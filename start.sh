b8140a6e846cd39e7189fe705276e367ac98d0d9
red3aaa <m15013049151@gmail.com>
2025-05-02 21:55:01 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 sudo cloudflared service install ${cloudflare_token}
 
 ./sync_data.sh &
-if [[ "${DOWNLOAD_BACKUP}" == "true" ]]; then
+if [ "$DOWNLOAD_BACKUP" = "true" ]; then
 	sleep 15
 fi
 ./forgejo-11.0.0-linux-amd64
