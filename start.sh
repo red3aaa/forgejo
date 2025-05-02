59f80bb0685a5a272d790f8ef957941de22fb614
red3aaa <m15013049151@gmail.com>
2025-05-02 23:55:08 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ sudo cloudflared service install ${cloudflare_token}
 
 if [ ! -d "/app/data/data" ]; then
 	sudo mkdir -p /app/data/data
-	sudo mkdir -p /app/data/custom
+	sudo mkdir -p /app/data/data/custom
 fi
 sudo chmod 777 /app/data/data
-sudo chmod 777 /app/data/custom
+sudo chmod 777 /app/data/data/custom
 sudo chmod 777 /app
 sudo chmod 777 /app/data
 
@@ -15,4 +15,4 @@ if [ "$DOWNLOAD_BACKUP" = "true" ]; then
 fi
 
 cd /app/data
-../forgejo-11.0.0-linux-amd64 --config /app/data/custom/app.ini --data-path /app/data/data
+../forgejo-11.0.0-linux-amd64 --config /app/data/data/custom/app.ini --data-path /app/data/data
